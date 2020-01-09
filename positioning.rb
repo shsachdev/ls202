3fe6a2668e3fe1a713e9a8640ef454eb1b552c45
Shikhar Sachdev <shsachdev@gmail.com>
2020-01-08 20:31:22 -0800

    positioning

---
+++ positioning.rb
@@ -0,0 +1,11 @@
+# offset properties work in conjuction with position to determine what direction
+# you want to move an element and how far. Each offset measures the inward distance
+# from the side of the container named by the offset property. For example, bottom: 50px
+# indicates a position 50px inward from (above) the bottom edge of the container.
+
+# The position property tells the browser how to position the selected elements.
+
+# Relative positioning moves an element to a new position relative to where the browser would otherwise put it.
+# For instance, if you include left: 50px and bottom: 100px with position: relative,
+# the browser will shift the element 50px inward from the left edge and 100px upward from the bottom edge
+#  from where the browser would place it otherwise.
